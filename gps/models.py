ccab9e4f6d84554a8a93f46a0d3685144f4ef596
Zachary Juang <zachary822@me.com>
2022-09-24 23:37:10 -0400

    More better pydantic integration

--- gps/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class WatchConfig(BaseModel):
-    enable: bool = True
-    json_: bool = Field(True, alias="json")
-    split24: bool = False
-    raw: int = 0
--- gpsd_client/client.py
+++ gpsd_client/client.py
@@ -1,6 +1,6 @@
 import asyncio
-import json
-from gps.models import WatchConfig
+
+from gpsd_client.schemas import Devices, Response, Version, WatchConfig
 
 POLL = "?POLL;\r\n"
 WATCH = "?WATCH={}\r\n"
@@ -9,6 +9,9 @@ WATCH = "?WATCH={}\r\n"
 class GpsdClient:
     reader: asyncio.StreamReader
     writer: asyncio.StreamWriter
+    version: Version
+    devices: Devices
+    watch: WatchConfig
 
     def __init__(self, host: str, port: int, watch_config: WatchConfig = WatchConfig()):
         self.host = host
@@ -20,17 +23,23 @@ class GpsdClient:
         self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
 
         self.writer.write(WATCH.format(self.watch_config.json(by_alias=True)).encode())
+        await self.writer.drain()
 
-        self.version = json.loads(await self.reader.readline())
-        self.devices = json.loads(await self.reader.readline())
-        self.watch = json.loads(await self.reader.readline())
+        self.version = await self.get_result()
+        self.devices = await self.get_result()
+        self.watch = await self.get_result()
 
     async def close(self):
         self.writer.close()
         await self.writer.wait_closed()
 
-    def begin_polling(self):
+    async def get_result(self):
+        return Response.parse_raw(await self.reader.readline()).__root__
+
+    async def poll(self):
         self.writer.write(POLL.encode())
+        await self.writer.drain()
+        return await self.get_result()
 
     async def __aenter__(self):
         await self.connect()
@@ -40,13 +49,12 @@ class GpsdClient:
         await self.close()
 
     def __aiter__(self):
-        self.begin_polling()
         return self
 
     async def __anext__(self):
-        result = json.loads(await self.reader.readline())
-        if result["class"] == "TPV":
+        result = await self.get_result()
+        if result.class_ == "TPV":
             return result
-        if result["class"] == "SKY":
+        if result.class_ == "SKY":
             self.sky = result
-        return await self.__anext__()
+        return await anext(self)
