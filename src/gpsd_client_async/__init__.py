6ab1dd7faffa75e65d2cfcb9e6d1ff5a63a9d4dc
Tomas Peterka <tomas.peterka@dronetag.cz>
2025-04-09 16:57:35 +0200

    feat: Bump version to 1.1.0

--- src/gpsd_client_async/__init__.py
+++ src/gpsd_client_async/__init__.py
@@ -1,7 +1,6 @@
 from .client import GpsdClient
-from .messages import Message
 from .messages import Sky as SkyMessage
 from .messages import TPV as TpvMessage
 
 
-__all__ = ["GpsdClient", "Message", "SkyMessage", "TpvMessage"]
+__all__ = ["GpsdClient", "SkyMessage", "TpvMessage"]
